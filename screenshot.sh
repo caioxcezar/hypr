11b43f35cb229d37a9a857cb153a9121293b326e
Caio Rezende <caiocezar50@outlook.com>
2026-05-24 19:37:29 -0300

    feat: sound on screenshot

--- screenshot.sh
+++ screenshot.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 path="$HOME/Pictures/Screenshots/screenshot-$(date '+%Y-%m-%d_%H-%M-%S-%3N').png"
 grim -g "$(slurp)" $path
-wl-copy < $path
+if [[ $? -eq 0 ]]; then
+    mpv --no-video  "$HOME/.config/hypr/sound-screenshot.mp3"
+    wl-copy < $path
+fi
