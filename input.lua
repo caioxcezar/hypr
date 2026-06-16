d982f968b8fb2d2498a68de0027a54bdfa1c7011
Caio Rezende <caiocezar50@outlook.com>
2026-06-16 00:37:33 -0300

    feat: add br layout

--- input.lua
+++ input.lua
@@ -28,11 +28,14 @@ hl.gesture({
 
 -- per-device config
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Devices/ for more
-local rk84 = {
+local rk84_en = {
 	kb_layout = "us",
 	kb_variant = "intl",
 	kb_model = "pc105",
 	kb_options = "cedilla:cedilla",
+}
+local rk84_br = {
+	kb_layout = "br",
 }
 local kanvas_pro = {
 	transform = 0,
@@ -40,8 +43,10 @@ local kanvas_pro = {
 	left_handed = true,
 }
 
-hl.device(Merge({ name = "compx-2.4g-wireless-receiver" }, rk84))
-hl.device(Merge({ name = "sino-wealth-rk-bluetooth-keyboar" }, rk84))
+hl.device(Merge({ name = "compx-2.4g-wireless-receiver" }, rk84_br))
+hl.device(Merge({ name = "sino-wealth-rk-bluetooth-keyboard" }, rk84_br))
+-- hl.device(Merge({ name = "compx-2.4g-wireless-receiver" }, rk84_en))
+-- hl.device(Merge({ name = "sino-wealth-rk-bluetooth-keyboar" }, rk84_en))
 
 hl.device(Merge({ name = "huion-huion-monitor-stylus" }, kanvas_pro))
 hl.device(Merge({ name = "huion-huion-monitor" }, kanvas_pro))
