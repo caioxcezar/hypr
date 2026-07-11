6a1516e832ca4e70c4e130336bfa2c3097443fde
Caio Rezende <caiocezar50@outlook.com>
2026-07-11 13:24:03 -0300

    update startup

--- autostart.lua
+++ autostart.lua
@@ -24,6 +24,10 @@ hl.on("hyprland.start", function()
 	hl.exec_cmd("flatpak run com.github.wwmm.easyeffects -w")
 	hl.exec_cmd("steam -silent")
 	hl.exec_cmd("ABDownloadManager --background")
-	hl.exec_cmd("flatpak run dev.vencord.Vesktop --start-minimized")
+	hl.exec_cmd(
+		'flatpak run dev.vencord.Vesktop --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.7727.56 Safari/537.36" --start-minimized'
+	)
 	hl.exec_cmd("albert")
+	hl.exec_cmd("jetbrains-toolbox --minimize")
+	hl.exec_cmd("nordvpn status >/dev/null 2>&1")
 end)
